dd98210f3da91b05347d345d1da538b1b4e9727f
Lucy812 <@Lucy812>
2022-08-28 23:18:06 +0300

    feat: add assets and data

---
+++ data/mythicCards/green/index.js
@@ -0,0 +1,135 @@
+//import greenCardsAssets from '../../../assets/MythicCards/green';
+
+const cards = {
+  green1: 'assets/MythicCards/green/green1.png',
+  green2: 'assets/MythicCards/green/green2.png',
+  green3: 'assets/MythicCards/green/green3.png',
+  green4: 'assets/MythicCards/green/green4.png',
+  green5: 'assets/MythicCards/green/green5.png',
+  green6: 'assets/MythicCards/green/green6.png',
+  green7: 'assets/MythicCards/green/green7.png',
+  green8: 'assets/MythicCards/green/green8.png',
+  green9: 'assets/MythicCards/green/green9.png',
+  green10: 'assets/MythicCards/green/green10.png',
+  green11: 'assets/MythicCards/green/green11.png',
+  green12: 'assets/MythicCards/green/green12.png',
+  green13: 'assets/MythicCards/green/green13.png',
+  green14: 'assets/MythicCards/green/green14.png',
+  green15: 'assets/MythicCards/green/green15.png',
+  green16: 'assets/MythicCards/green/green16.png',
+  green17: 'assets/MythicCards/green/green17.png',
+  green18: '.assets/MythicCards/green/green18.png',
+};
+
+const cardsData = [
+  {
+    id: 'green1',
+    cardFace: cards.green1,
+    difficulty: 'easy',
+    color:'green'
+  },
+  {
+    id: 'green2',
+    cardFace: cards.green2,
+    difficulty: 'hard',
+    color:'green'
+  },
+  {
+    id: 'green3',
+    cardFace: cards.green3,
+    difficulty: 'hard',
+    color:'green'
+  },
+  {
+    id: 'green4',
+    cardFace: cards.green4,
+    difficulty: 'hard',
+    color:'green'
+  },
+  {
+    id: 'green5',
+    cardFace: cards.green5,
+    difficulty: 'hard',
+    color:'green'
+  },
+  {
+    id: 'green6',
+    cardFace: cards.green6,
+    difficulty: 'hard',
+    color:'green'
+  },
+  {
+    id: 'green7',
+    cardFace: cards.green7,
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green8',
+    cardFace: cards.green8,
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green9',
+    cardFace: cards.green9,
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green10',
+    cardFace: cards.green10,
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green11',
+    cardFace: cards.green11,
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green12',
+    cardFace: cards.green12,
+    difficulty: 'easy',
+    color:'green'
+  },
+  {
+    id: 'green13',
+    cardFace: cards.green13,
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green14',
+    cardFace: cards.green14,
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green15',
+    cardFace: cards.green15,
+    difficulty: 'normal',
+    color:'green'
+  },
+  {
+    id: 'green16',
+    cardFace: cards.green16,
+    difficulty: 'easy',
+    color:'green'
+  },
+  {
+    id: 'green17',
+    cardFace: cards.green17,
+    difficulty: 'easy',
+    color:'green'
+  },
+  {
+    id: 'green18',
+    cardFace: cards.green18,
+    difficulty: 'easy',
+    color:'green'
+  },
+]
+
+export {cardsData};
